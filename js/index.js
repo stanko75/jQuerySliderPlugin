21f308f3f2937824267c22e00a1e59ef17508a84
stanko75 <Genije123>
2015-12-26 22:22:01 +0100

    Make sure that images are loaded

--- js/index.js
+++ js/index.js
@@ -12,43 +12,51 @@
             secondHalfId = "#" + secondHalf,
             widthOfFirstHalf = 0,
             widthOfSecondHalf = 0,
-            spaceBetweenTwoHalfs = 10;
+            spaceBetweenTwoHalfs = 10,
+            loadedImgIndex = 0,
+            numberOfImages = 0;
 
         myQ("<span class='imagesTrack' id='" + firstHalf + "'/>").appendTo("#container");
         myQ("<span class='imagesTrack' id='" + secondHalf + "'/>").appendTo("#container");
 
         data.forEach(function (file, index, array) {
-            var imgSrc = '<img src="' + file + '">';
+            var imgSrc = '<img src="' + file + '" id="' + index + '">';
+            numberOfImages = numberOfImages + 1;
 
             if (index < array.length / 2) {
                 myQ(imgSrc).appendTo(firstHalfId);
             } else {
                 myQ(imgSrc).appendTo(secondHalfId);
             }
+
+            myQ('#' + index).on("load", function () {
+                loadedImgIndex = loadedImgIndex + 1;
+            });
         });
 
         secondHalfRightPosition = -spaceBetweenTwoHalfs;
 
         setInterval(function () {
-            widthOfFirstHalf = myQ("#first-half").width();
-            widthOfSecondHalf = myQ("#second-half").width();
+            if (loadedImgIndex === numberOfImages) {
+                widthOfFirstHalf = myQ("#first-half").width();
+                widthOfSecondHalf = myQ("#second-half").width();
 
-            px = firstHalfRightPosition + "px";
-            myQ(firstHalfId).css("right", px);
-            firstHalfRightPosition = firstHalfRightPosition + 1;
+                px = firstHalfRightPosition + "px";
+                myQ(firstHalfId).css("right", px);
+                firstHalfRightPosition = firstHalfRightPosition + 1;
 
-            px = secondHalfRightPosition + "px";
-            myQ(secondHalfId).css("right", px);
-            secondHalfRightPosition = secondHalfRightPosition + 1;
+                px = secondHalfRightPosition + "px";
+                myQ(secondHalfId).css("right", px);
+                secondHalfRightPosition = secondHalfRightPosition + 1;
 
-            if (firstHalfRightPosition === widthOfFirstHalf + spaceBetweenTwoHalfs) {
-                firstHalfRightPosition = -1 * secondHalfRightPosition - spaceBetweenTwoHalfs;
+                if (firstHalfRightPosition === widthOfFirstHalf + spaceBetweenTwoHalfs) {
+                    firstHalfRightPosition = -1 * secondHalfRightPosition - spaceBetweenTwoHalfs;
+                }
+
+                if (secondHalfRightPosition === widthOfFirstHalf + widthOfSecondHalf + spaceBetweenTwoHalfs) {
+                    secondHalfRightPosition = -1 * firstHalfRightPosition - spaceBetweenTwoHalfs;
+                }
             }
-
-            if (secondHalfRightPosition === widthOfFirstHalf + widthOfSecondHalf + spaceBetweenTwoHalfs) {
-                secondHalfRightPosition = -1 * firstHalfRightPosition - spaceBetweenTwoHalfs;
-            }
-
         }, 1);
 
     });
